893c423b12ee537aa8658e6f1409124760566acb
Stephan Köninger <stephan.koeninger@codecentric.de>
2021-02-21 12:47:33 +0100

    initial commit

---
+++ spring-boot-2.4/src/main/java/com/codecentric/boot/sample/Application.java
@@ -0,0 +1,13 @@
+package com.codecentric.boot.sample.v23x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
