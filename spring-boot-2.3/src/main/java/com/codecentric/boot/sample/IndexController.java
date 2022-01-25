6a10b92f430ad9e55f703696401d1f5b8ea6445d
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-01-25 21:46:30 +0100

    * add pipeline * add spring 3.0.0-M1

--- spring-boot-2.3/src/main/java/com/codecentric/boot/sample/IndexController.java
+++ spring-boot-2.3/src/main/java/com/codecentric/boot/sample/IndexController.java
@@ -3,10 +3,24 @@ package com.codecentric.boot.sample;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @RestController
 public class IndexController {
     @RequestMapping("/")
     public String index() {
-        return "Spring Boot Example";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        try (InputStream resourceStream = loader.getResourceAsStream("application.properties")) {
+            StringBuilder sb = new StringBuilder();
+            for (int ch; (ch = resourceStream.read()) != -1; ) {
+                sb.append((char) ch);
+            }
+            return "<pre><code>" + sb + "</code></pre>";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
