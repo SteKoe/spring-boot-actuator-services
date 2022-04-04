8577e29891b64e872b039941c56c444d23f734d5
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-04-04 15:53:13 +0200

    chore(spring-boot): update spring boot versions and fix 1.4 example

--- landing-page/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "./index.css"
-import "bulma/css/bulma.min.css";
-import "@fortawesome/fontawesome-free/js/all.min"
-
-const app = document.getElementById("app");
-ReactDOM.render(<App />, app);
